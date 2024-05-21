4675b21f61e628c25dced4370b0965eee95a32e1
Sebastian Schlegel <johann.schlegel@adesso.de>
2024-05-21 15:40:21 +0200

    3.22

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { CoursesCardListComponent } from '../courses-card-list/courses-card-list
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
 import { MessagesService } from '../messages/messages.service';
+import { CoursesStore } from '../services/courses.store';
 
 
 @Component({
@@ -22,9 +23,7 @@ export class HomeComponent implements OnInit {
 
 
   constructor(
-    private coursesService: CoursesService,
-    private loadingService: LoadingService,
-    private messagesService: MessagesService
+    private coursesStore: CoursesStore
   ) {
 
   }
@@ -35,30 +34,8 @@ export class HomeComponent implements OnInit {
 
 
   reloadCourses() {
-    this.loadingService.loadingOn();
-
-    const courses$ = this.coursesService.loadAllCourses()
-      .pipe(
-        map(courses => courses.sort(sortCoursesBySeqNo)),
-        catchError(err => {
-          const message = "Could not load courses";
-          console.log(message, err);
-          this.messagesService.showErrors(message);
-          return throwError(err);
-        })
-      );
-
-    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
-
-    this.beginnerCourses$ = loadCourses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == "BEGINNER"))
-      );
-
-    this.advancedCourses$ = loadCourses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == "ADVANCED"))
-      );
+    this.beginnerCourses$ = this.coursesStore.filterByCategory("BEGINNER");
+    this.advancedCourses$ = this.coursesStore.filterByCategory("ADVANCED");
   }
 }
 
