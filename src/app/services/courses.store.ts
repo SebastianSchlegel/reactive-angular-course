bd6fc2ad168d1b93011d23581d6cc9b65db00350
Sebastian Schlegel <johann.schlegel@adesso.de>
2024-05-21 15:58:00 +0200

    3.23

--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -1,23 +1,49 @@
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable, throwError } from "rxjs";
 import { Course, sortCoursesBySeqNo } from "../model/course";
-import { map } from "rxjs/operators";
+import { catchError, map, tap } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
 
 
 @Injectable({
-    providedIn: 'root' // make it globally avaialble
+    providedIn: 'root' // make it a globally avaialble singleton
 })
 export class CoursesStore {
 
-    courses$: Observable<Course[]>;
+    private subject = new BehaviorSubject<Course[]>([]);
+
+    courses$: Observable<Course[]> = this.subject.asObservable();
+
+    constructor(private http: HttpClient, private loadingService: LoadingService, private messagesService: MessagesService) {
+        this.loadAllCourses();
+    }
+
+    loadAllCourses() {
+        const loadCourses$ = this.http.get<Course[]>('/api/courses')
+            .pipe(
+                map(response => response["payload"]),
+                catchError(err => {
+                    const message = "Could not load courses";
+                    console.log(message, err);
+                    this.messagesService.showErrors(message);
+                    return throwError(err);
+                }
+                ),
+                tap(courses => this.subject.next(courses))
+            );
+
+        this.loadingService.showLoaderUntilCompleted(loadCourses$).subscribe();
+    }
 
     filterByCategory(category: string): Observable<Course[]> {
         return this.courses$
-        .pipe(
-            map(courses => 
-                courses.filter(course => course.category == category)
-            .sort(sortCoursesBySeqNo))
-        );
+            .pipe(
+                map(courses =>
+                    courses.filter(course => course.category == category)
+                        .sort(sortCoursesBySeqNo))
+            );
     }
 
 }
